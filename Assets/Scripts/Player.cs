ac8355c41f690f67d5c1fc8f683127bb0c4528e3
Conner Jowitt <conner.jowitt@infinityworks.com>
2021-03-26 19:34:53 +0000

    basic movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float _speed = 5;
+    
+    Vector2 targetVector = Vector2.zero;
+    Rigidbody2D _rb;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate(){
+        _rb.MovePosition(_rb.position + targetVector * _speed * Time.fixedDeltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetVector.x = Input.GetAxisRaw("Horizontal");
+        targetVector.y = Input.GetAxisRaw("Vertical");
+    }
+}
