3b745d9b49f44ce097e8a70243914b3a4d863080
Conner Jowitt <conner.jowitt@infinityworks.com>
2021-03-29 19:47:39 +0100

    basic sacrifice menu set up

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,16 @@ public class Player : MonoBehaviour
     [SerializeField] float _speed = 5;
     [SerializeField] float _shootDelay = 0.25F;
     [SerializeField] Rigidbody2D _bulletPrefab;
-    
+    [SerializeField] Canvas _sacrificeMenu;
     Vector2 targetVector = Vector2.zero;
     Vector3 direction;
     Vector3 velocity;
     Rigidbody2D _rb;
     float _nextFireTime;
     float angleOffset = -90;
+    bool _lostArm = false;
+    bool _lostLeg = false;
+    bool _canShoot = true;
 
     void Awake()
     {
@@ -35,20 +38,44 @@ public class Player : MonoBehaviour
         targetVector.y = Input.GetAxisRaw("Vertical");
         targetVector.Normalize();
 
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            _sacrificeMenu.gameObject.SetActive(!_sacrificeMenu.gameObject.activeSelf);
+        }
+
         if(ReadyToFire())
         {
             Fire();
         }
     }
 
-    bool ReadyToFire() => Time.time >= _nextFireTime;
+    bool ReadyToFire() => (Time.time >= _nextFireTime) && _canShoot;
 
     void Fire()
     {
         _nextFireTime = Time.time + _shootDelay;
         var shot = Instantiate(_bulletPrefab, transform.position + Vector3.forward, transform.rotation);
-        Debug.Log(velocity);
-        Debug.Log(velocity + transform.up * 5);
         shot.velocity = velocity + (transform.up * 15);
+    }
+
+    public void LoseLeg()
+    {
+        if(!_lostLeg)
+        {
+            _speed /= 2;
+            _lostLeg = true;
+        }
+        else
+            _speed = 0;
+    }
+    public void LoseArm()
+    {
+        if(!_lostArm)
+        {
+            _shootDelay *= 2;
+            _lostArm = true;
+        }
+        else
+            _canShoot = false;
     }
 }
